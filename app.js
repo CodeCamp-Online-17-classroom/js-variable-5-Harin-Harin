8fbef02b4c4d4ef525a8c86d5ee194cc18e4245e
Harin-Mabao <mixharinmabao@gmail.com>
2024-06-24 01:03:43 +0700

    Update app Rev00 first issued.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-// write code here
+const firstName = "John";
+const lastName = "Doe";
+const age = 27;
+const address = "118 Mint Tower Banthatthong Rd.";
+
+console.log(`${firstName} ${lastName} ${age} ${address}`)
+alert(`${firstName} ${lastName} ${age} ${address}`)
